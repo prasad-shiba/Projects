c11c35000735600be11b05d0b2edf2e1845faf28
Shiba Prasad <shibaprasadsahu943@gmail.com>
2022-08-29 13:24:01 +0530

    added

--- Number Shifting Game/number_shifting_game.c
+++ Number Shifting Game/number_shifting_game.c
@@ -2,55 +2,77 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <string.h>
+#include <time.h>
 
-const char RED_COLOR[] = "\x1b[31m";
-const char GREEN_COLOR[] = "\x1b[32m";
-const char YELLOW_COLOR[] = "\x1b[33m";
-const char BLUE_COLOR[] = "\x1b[34m";
-const char WHITE_COLOR[] = "\033[0;37m";
-const char DEFAULT_COLOR[] = "\x1b[0m";
+#define numOW_DOWN_KEY 80
+#define numOW_UP_KEY 72
+#define numOW_LEFT_KEY 75
+#define numOW_RIGHT_KEY 77
+#define E 69
+#define e 101
+
+const char COLOR_RED[] = "\x1b[31m";
+const char COLOR_GREEN[] = "\x1b[32m";
+const char COLOR_YELLOW[] = "\x1b[33m";
+const char COLOR_BLUE[] = "\x1b[34m";
+const char COLOR_WHITE[] = "\033[0;37m";
+const char COLOR_DEFAULT[] = "\x1b[0m";
 int remaining_moves;
+int matrix[4][4];
+char *username;
 void printWelcomeScreen(const char *USERNAME);
 void printColor(const char *COLOR);
 void clearScreen();
 void pauseScreen();
 void pressAnyKeyToStart();
 void printWinningMatrix();
-void askUserToPlayGameOrNot();
+int askUserToPlayGameOrNot();
 void startGame();
 void gameLose();
 void gameWin();
 void restartGame();
 char *getUsername();
+int getEnteredKey();
+void onDownArrowKeyPressed();
+void onLeftArrowKeyPressed();
+void onRightArrowKeyPressed();
+void onUpArrowKeyPressed();
+void printRandomMatrix();
+void printMatrix();
+
+int getEnteredKey()
+{
+    fflush(stdin);
+    return getch();
+}
 
 char *getUsername()
 {
     char *ch = (char *)malloc(30 * sizeof(char));
+    // pauseScreen();
     clearScreen();
     printf("\n\n\t\t\t\tPlayer Name :   ");
     fgets(ch, 30, stdin);
     ch[strlen(ch) - 1] = '\0';
+    username = ch;
     return ch;
 }
 
-void printColor(const char *COLOR)
+void printColor(const char *COLOR) { printf("%s", COLOR); }
+
+void clearScreen() { system("cls"); }
+
+void pauseScreen() { system("pause"); }
+
+void restartGame()
 {
-    printf("%s", COLOR);
+    clearScreen();
+    void startGame();
 }
 
-void clearScreen()
+void pressAnyKeyToStart(const char *message)
 {
-    system("cls");
-}
-
-void pauseScreen()
-{
-    system("pause");
-}
-
-void pressAnyKeyToStart(const char *str)
-{
-    printf(str);
+    printf(message);
     _getch();
 }
 
@@ -59,74 +81,122 @@ void printWinningMatrix()
     int k = 1;
     for (int i = 0; i < 4; ++i)
     {
-        printf("\n\t\t\t\t|");
+        printf("\n\t\t\t\t\t\t|");
         for (int j = 0; j < 4; ++j)
         {
             if (k != 16)
             {
-                printf(" %d ", k);
+                printf(" %2d ", k);
                 ++k;
+                printf("|");
             }
             else
             {
-                printf("   ");
+                printf("    |");
             }
         }
-        printf("|");
     }
 }
 
-void askUserToPlayGameOrNot()
+int askUserToPlayGameOrNot()
 {
-    char ch = 'd';
+    fflush(stdin);
+    char ch;
+
     do
     {
-        printf("\n\nAre you want to restart the game ? (y / n)  :  ");
-        ch = _getch();
-    } while (ch != 'y' || ch != 'n');
-    if (ch == 'y')
+        printf("\n\n\t\t\t\t\t    Are you want to restart the game ? (y / n)  :  ");
+        scanf("%c", &ch);
+    } while (ch != 'y' && ch != 'n' && ch != 'Y' && ch != 'N');
+    // printf("%c", ch);
+    if (ch == 'y' || ch == 'Y')
     {
-        restartGame();
+        return 1;
     }
-    else
+    else if (ch == 'n' || ch == 'Y')
     {
-        exit(0);
+        return 0;
     }
 }
 
 void startGame()
 {
-    remaining_moves = 4;
+    clearScreen();
     int isGameWin = 0;
-    if (isGameWin == 1)
+    while (1)
     {
-        gameWin();
-    }
-    else
-    {
-        gameLose();
+        remaining_moves = 4;
+        printf("\n\n\n\t\t\t\t\t    %s  , Remaining moves - %d", username, remaining_moves);
+        printRandomMatrix(matrix);
+        int check;
+        while (remaining_moves >= 1)
+        {
+            check = getEnteredKey();
+            switch (check)
+            {
+            case E:
+                exit(0);
+                break;
+            case e:
+                exit(0);
+                break;
+            case numOW_DOWN_KEY:
+                onDownArrowKeyPressed();
+                break;
+            case numOW_LEFT_KEY:
+                onLeftArrowKeyPressed();
+                break;
+            case numOW_RIGHT_KEY:
+                onRightArrowKeyPressed();
+                break;
+            case numOW_UP_KEY:
+                onUpArrowKeyPressed();
+                break;
+            }
+        }
+        if (isGameWin == 1)
+        {
+            gameWin();
+        }
+        else
+        {
+            gameLose();
+        }
     }
 }
 
 void gameLose()
 {
-    printColor(RED_COLOR);
-    printf("\n\n\n\t\t\t\tYou lose the Game !");
-    printColor(DEFAULT_COLOR);
-    askUserToPlayGameOrNot();
+    clearScreen();
+    printColor(COLOR_RED);
+    printf("\n\n\n\t\t\t\t\t\t\tYou lose the Game !\n");
+    printColor(COLOR_DEFAULT);
+    if (askUserToPlayGameOrNot())
+    {
+        restartGame();
+    }
+    else
+    {
+        clearScreen();
+        exit(0);
+    }
 }
 
 void gameWin()
 {
-    printColor(GREEN_COLOR);
-    printf("\n\n\n\t\t\t\tYou won the Game !");
-    printColor(DEFAULT_COLOR);
-    askUserToPlayGameOrNot();
-}
-
-void restartGame()
-{
-    void startGame();
+    clearScreen();
+    printColor(COLOR_GREEN);
+    printf("\n\n\n\t\t\t\t\t\t\tYou won the Game !\n");
+    printColor(COLOR_DEFAULT);
+    if (askUserToPlayGameOrNot())
+    {
+        restartGame();
+    }
+    else
+    {
+        clearScreen();
+        exit(0);
+    }
 }
 
 void printWelcomeScreen(const char *USERNAME)
@@ -134,28 +204,109 @@ void printWelcomeScreen(const char *USERNAME)
     printf("\n\t\t\t%s ,Welcome to NUMBER_SHIFTING_GAME\n\n", USERNAME);
     pressAnyKeyToStart("\t\t\t\tEnter any key to start....    ");
     clearScreen();
-    printColor(YELLOW_COLOR);
+    printColor(COLOR_YELLOW);
     printf("\t\t\t\t\t\t ----------------------\n\t\t\t\t\t\t| NUMBER SHIFTING GAME |\n\t\t\t\t\t\t ----------------------\n\n");
     printf("\t\t\t\t\t\t   RULES OF THIS GAME\n\t\t\t\t\t\t   ------------------\n\n\n");
-    printColor(RED_COLOR);
-    printf("\t\t\t\t1. You can move only 1 step at a time by arrow key\n\n");
-    printColor(BLUE_COLOR);
-    printf("\t\t\t\t\tMove Up    :  by Up arrow key\n");
-    printf("\t\t\t\t\tMove Down  :  by Down arrow key\n");
-    printf("\t\t\t\t\tMove Left  :  by Left arrow key\n");
-    printf("\t\t\t\t\tMove Right :  by Right arrow key\n");
-    printColor(RED_COLOR);
+    printColor(COLOR_RED);
+    printf("\t\t\t\t1. You can move only 1 step at a time by numow key\n\n");
+    printColor(COLOR_BLUE);
+    printf("\t\t\t\t\tMove Up    :  by Up numow key\n");
+    printf("\t\t\t\t\tMove Down  :  by Down numow key\n");
+    printf("\t\t\t\t\tMove Left  :  by Left numow key\n");
+    printf("\t\t\t\t\tMove Right :  by Right numow key\n");
+    printColor(COLOR_RED);
     printf("\n\t\t\t\t2. You Can move number at empty position only\n");
-    printf("\n\t\t\t\t3. For each valid move : your total number of move will decrease by 1\n");
+    printf("\n\t\t\t\t3. For each valid move : your total number of move will decrease by 1");
     printf("\n\t\t\t\t4. Winning situation : Number in a 4x4 matrix should be in order from 1 to 15\n\n\n");
-    printColor(YELLOW_COLOR);
-    printf("\n\t\t\t\t\t\t   Winning Situation\n\t\t\t\t\t\t   -----------------\n\n\n\t\t\t\t\t\t---------------------\n\n\n");
+    printColor(COLOR_YELLOW);
+    printf("\n\t\t\t\t\t\t   Winning Situation\n\t\t\t\t\t\t   -----------------\n\n\n\t\t\t\t\t\t -------------------");
     printWinningMatrix();
-    printColor(RED_COLOR);
+    printf("\n\t\t\t\t\t\t -------------------");
+    printColor(COLOR_RED);
     printf("\n\n\t\t\t\t5. You can exit game at any time by pressing 'E' or 'e'\n\n");
-    printColor(DEFAULT_COLOR);
+    printColor(COLOR_DEFAULT);
     printf("\t\t\t\tSo Try to win in minimum no of move\n\n\n");
     printf("\t\t\t\t\tHappy gaming , Good Luck\n\n");
     pressAnyKeyToStart("\t\t\t\tEnter any key to start game....    ");
     startGame();
+}
+
+void printMatrix()
+{
+    printColor(COLOR_YELLOW);
+    printf("\n\n\n\t\t\t\t\t\t -------------------");
+    for (int i = 0; i < 4; ++i)
+    {
+        printf("\n\t\t\t\t\t\t|");
+        for (int j = 0; j < 4; ++j)
+        {
+            if (matrix[i][j] == 0)
+            {
+                printf("    |");
+            }
+            else
+            {
+                printf(" %2d ", matrix[i][j]);
+                printf("|");
+            }
+        }
+    }
+    printf("\n\t\t\t\t\t\t -------------------\n\n");
+    printColor(COLOR_DEFAULT);
+}
+
+void printRandomMatrix()
+{
+    int size = 15, lastindex = 14, index;
+    int num[size - 1];
+    for (int i = 0; i < size; ++i)
+        num[i] = i + 1;
+    srand(time(NULL));
+    for (int i = 0; i < 4; ++i)
+    {
+        for (int j = 0; j < 4; ++j)
+        {
+            if (lastindex >= 0)
+            {
+                index = rand() % (lastindex + 1);
+                matrix[i][j] = num[index];
+                num[index] = num[lastindex--];
+            }
+        }
+    }
+    matrix[3][3] = 0;
+    printMatrix(matrix);
+}
+
+void onDownArrowKeyPressed()
+{
+    --remaining_moves;
+    clearScreen();
+    printf("\n\n\n\t\t\t\t\t    %s  , Remaining moves - %d", username, remaining_moves);
+    printMatrix();
+    printf("down key");
+}
+void onLeftArrowKeyPressed()
+{
+    --remaining_moves;
+    clearScreen();
+    printf("\n\n\n\t\t\t\t\t    %s  , Remaining moves - %d", username, remaining_moves);
+    printMatrix();
+    printf("left key");
+}
+void onRightArrowKeyPressed()
+{
+    --remaining_moves;
+    clearScreen();
+    printf("\n\n\n\t\t\t\t\t    %s  , Remaining moves - %d", username, remaining_moves);
+    printMatrix();
+    printf("right key");
+}
+void onUpArrowKeyPressed()
+{
+    --remaining_moves;
+    clearScreen();
+    printf("\n\n\n\t\t\t\t\t    %s  , Remaining moves - %d", username, remaining_moves);
+    printMatrix();
+    printf("up key");
 }
